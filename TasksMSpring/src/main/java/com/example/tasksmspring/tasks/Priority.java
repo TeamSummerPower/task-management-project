2cdc8e5ed853a97689e769c3131c2edc7ba658e1
v-petrov <vapetrov@tu-sofia.bg>
2023-07-17 21:32:57 +0300

    fixing

--- TasksMSpring/src/main/java/com/example/tasksmspring/tasks/Priority.java
+++
@@ -1,8 +0,0 @@
-package com.example.tasksmspring.tasks;
-
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-
-@JsonSerialize
-public enum Priority {
-    NOT_IMPORTANT, IMPORTANT, VERY_IMPORTANT, MANDATORY;
-}
--- TasksMSpring/src/main/java/com/example/tasksmspring/users/Role.java
+++
@@ -1,8 +0,0 @@
-package com.example.tasksmspring.users;
-
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-
-@JsonSerialize
-public enum Role {
-    USER, ADMIN, OWNER;
-}
